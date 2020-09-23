e94db44a06bb38db590f195275e20a44ed5c4b63
Alexandre Marco <alexandre.marco@queroquitar.com.br>
2020-09-23 19:41:09 -0300

    ...

--- soap/soap.go
+++ soap/soap.go
@@ -320,7 +320,7 @@ func (s *Client) call(ctx context.Context, soapAction string, request, response 
 			req.Header.Set(k, v)
 		}
 	}
-	req.Close = true
+	//req.Close = true
 
 	client := s.opts.client
 	if client == nil {
